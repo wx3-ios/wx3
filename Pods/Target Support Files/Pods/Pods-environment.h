aa4ac521a557d73b548932eff5dca9cd81dcae28
roderick <2419011393@qq.com>
2015-03-18 17:51:20 +0800

    pod 项目合并成功

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -96,6 +96,12 @@
 #define COCOAPODS_VERSION_MINOR_OpenUDID 0
 #define COCOAPODS_VERSION_PATCH_OpenUDID 0
 
+// Reachability
+#define COCOAPODS_POD_AVAILABLE_Reachability
+#define COCOAPODS_VERSION_MAJOR_Reachability 3
+#define COCOAPODS_VERSION_MINOR_Reachability 2
+#define COCOAPODS_VERSION_PATCH_Reachability 0
+
 // RegexKitLite
 #define COCOAPODS_POD_AVAILABLE_RegexKitLite
 #define COCOAPODS_VERSION_MAJOR_RegexKitLite 4
