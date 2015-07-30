770f5a3505a8259287bb338ca14d531f1db00224
Gary <1667906749@qq.com>
2015-07-30 10:28:27 +0800

    商品详情加入库存不满足购买数量的判断

--- RKWXT/GoodsInfo/NewGoodsInfoVC.m
+++ RKWXT/GoodsInfo/NewGoodsInfoVC.m
@@ -616,6 +616,10 @@
         [UtilTool showAlertView:@"该套餐已售完"];
         return;
     }
+    if(entity.stockNumber < rightView.goodsNum){
+        [UtilTool showAlertView:[NSString stringWithFormat:@"该套餐库存不足%d件",rightView.goodsNum]];
+        return;
+    }
     entity.buyNumber = (rightView.goodsNum<=0?1:rightView.goodsNum);
     [goodsArr addObject:entity];
     [[CoordinateController sharedCoordinateController] toMakeOrderVC:self orderInfo:goodsArr animated:YES];
