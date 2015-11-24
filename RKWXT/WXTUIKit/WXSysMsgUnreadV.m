40e7325cc9fdbf03185f5915cdeff492dcf9cca4
songhb <1667906749@qq.com>
2015-11-24 15:51:32 +0800

    首页顶部消息和分类加入文字

--- RKWXT/WXTUIKit/WXSysMsgUnreadV.m
+++ RKWXT/WXTUIKit/WXSysMsgUnreadV.m
@@ -30,20 +30,36 @@
 {
     self = [super initWithFrame:frame];
     if (self) {
-        WXUIButton *button = [WXUIButton buttonWithType:UIButtonTypeCustom];
-        [button setFrame:CGRectMake(0, 0, frame.size.width, frame.size.height)];
         UIImage *btnImg = [UIImage imageNamed:@"sysPushMessageIcon.png"];
         CGSize btnSize = btnImg.size;
-        [button setImage:btnImg forState:UIControlStateNormal];
-        [button addTarget:self action:@selector(toUnreadSysMsg) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:button];
+        
+        WXUIButton *leftBtn = [WXUIButton buttonWithType:UIButtonTypeCustom];
+        leftBtn.frame = CGRectMake(20, 2, 60, 40);
+        [leftBtn setImage:btnImg forState:UIControlStateNormal];
+        [leftBtn setTitle:@"消息" forState:UIControlStateNormal];
+        [leftBtn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        [leftBtn.titleLabel setFont:WXFont(10.0)];
+        [leftBtn addTarget:self action:@selector(toUnreadSysMsg) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:leftBtn];
+        
+        CGPoint buttonBoundsCenter = CGPointMake(CGRectGetMidX(leftBtn.titleLabel.bounds), CGRectGetMidY(leftBtn.titleLabel.bounds));
+        CGPoint endImageViewCenter = CGPointMake(buttonBoundsCenter.x, CGRectGetMidY(leftBtn.imageView.bounds));
+        CGPoint endTitleLabelCenter = CGPointMake(buttonBoundsCenter.x, CGRectGetHeight(leftBtn.bounds)-CGRectGetMidY(leftBtn.titleLabel.bounds));
+        CGPoint startImageViewCenter = leftBtn.imageView.center;
+        CGPoint startTitleLabelCenter = leftBtn.titleLabel.center;
+        CGFloat imageEdgeInsetsLeft = endImageViewCenter.x - startImageViewCenter.x;
+        CGFloat imageEdgeInsetsRight = -imageEdgeInsetsLeft;
+        leftBtn.imageEdgeInsets = UIEdgeInsetsMake(0, imageEdgeInsetsLeft, 40/3, imageEdgeInsetsRight);
+        CGFloat titleEdgeInsetsLeft = endTitleLabelCenter.x - startTitleLabelCenter.x;
+        CGFloat titleEdgeInsetsRight = -titleEdgeInsetsLeft;
+        leftBtn.titleEdgeInsets = UIEdgeInsetsMake(40*2/3-5, titleEdgeInsetsLeft, 0, titleEdgeInsetsRight);
         
         UIImage *image = [UIImage imageNamed:@"unreadBg.png"];
         CGSize imgSize = image.size;
         _unreadNumberImgV = [[WXUIImageView alloc] initWithImage:image];
         CGRect unreadViewRect = CGRectMake(-imgSize.width*0.3 + (frame.size.width-btnSize.width)/2.0, (frame.size.height-btnSize.height)/2.0-imgSize.height*0.3, imgSize.width, imgSize.height);
         [_unreadNumberImgV setFrame:unreadViewRect];
-        [button addSubview:_unreadNumberImgV];
+        [leftBtn addSubview:_unreadNumberImgV];
         
         _unreadLabel = [[WXUILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 20)];
         [_unreadLabel setFont:[UIFont systemFontOfSize:9.0]];
