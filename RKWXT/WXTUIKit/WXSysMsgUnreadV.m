a2e1717b77aa6b9d54c2d427d6c9a0b8da7c448e
songhb <1667906749@qq.com>
2015-12-01 09:23:29 +0800

    商品详情页面加入限时购抢购时间判断

--- RKWXT/WXTUIKit/WXSysMsgUnreadV.m
+++ RKWXT/WXTUIKit/WXSysMsgUnreadV.m
@@ -57,7 +57,7 @@
         UIImage *image = [UIImage imageNamed:@"unreadBg.png"];
         CGSize imgSize = image.size;
         _unreadNumberImgV = [[WXUIImageView alloc] initWithImage:image];
-        CGRect unreadViewRect = CGRectMake(-imgSize.width*0.3 + (frame.size.width-btnSize.width)/2.0, (frame.size.height-btnSize.height)/2.0-imgSize.height*0.3, imgSize.width, imgSize.height);
+        CGRect unreadViewRect = CGRectMake(-imgSize.width*0.3 + (frame.size.width-btnSize.width)/2.0-5, (frame.size.height-btnSize.height)/2.0-imgSize.height*0.3-10, imgSize.width, imgSize.height);
         [_unreadNumberImgV setFrame:unreadViewRect];
         [leftBtn addSubview:_unreadNumberImgV];
         
@@ -71,6 +71,7 @@
 }
 
 - (void)setUnreadNumber:(NSInteger)number{
+    number = 1;
     [_unreadNumberImgV setHidden:number <= 0];
     if(number > 0){
         NSString *text = [NSString stringWithFormat:@"%d",(int)number];
